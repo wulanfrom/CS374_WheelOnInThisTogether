b43c74a86d9fb9fedb32df6965de6219495c2039
stdeguzman <seantristan@kaist.ac.kr>
2020-06-07 20:46:15 +0900

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,20 +1,29 @@
-function bindEvents() {
-	$(".icon-star").onclick {
-		//close sidebar
-		console.log("Closing sidebar");
-	}
+$( document ).ready(function() {		
+	function bindEvents() {
+		var SideBar = document.getElementById('sideBarClose');
+		var Options = document.getElementsByClassName('options');
 
-	$(".options").onclick {
-		var chosenTask = $(".options").html();
-		if (chosenTask == "Forum") {
-			//go to Forum page
+		SideBar.onclick = function() { //close sidebar
+			console.log("Closing sidebar");
+			// window.open(url);
+		}
+
+		Options[0].onclick = function() { // go to Forum page
 			console.log("Proceeding to Forum page");
-		} else if (chosenTask == "Routes") {
-			//go to Routes page
+			window.open("forum_homepage.html", "_self");
+		}
+
+		Options[1].onclick = function() { // go to Routes page
 			console.log("Proceeding to Routes page");
-		} else if (chosenTask == "Rate") {
-			//go to Rates page
+			// window.open(url);
+		}
+
+		Options[2].onclick = function() { // go to Rates page
 			console.log("Proceeding to Rates page");
+			window.open("rates.html", "_self");
 		}
 	}
-}
+
+	bindEvents();
+});
+
