054f72a3dcc7dd81648f427c83f6d54322321b55
stdeguzman <seantristan@kaist.ac.kr>
2020-06-07 20:46:25 +0900

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,11 +1,19 @@
-function bindEvents() {
-	$(".icon-star").onclick {
-		//open sidebar
-		console.log("Opening sidebar");
+$( document ).ready(function() {	
+	var Logo = document.getElementById('logo');
+	var SideBar = document.getElementById('sideBarOpen');
+	var wholePage = document.getElementsByClassName('outside_topbar');
+
+	function bindEvents() {
+		Logo.onclick = function() {
+			console.log("Return to Homepage");
+			window.open("homepage.html", "_self");
+		}
+		SideBar.onclick = function() {
+			console.log("Open the sidebar");
+			// $(className).style.opacity = "0.1";
+			window.open("sidebar.html", "_self");
+		}
 	}
 
-	$(".col-md-4").onclick {
-		//go to Homepage
-		console.log("Proceeding to Homepage");
-	}
-}
+	bindEvents();
+});
