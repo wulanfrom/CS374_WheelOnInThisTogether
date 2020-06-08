bb586cd458f84cc13bcc56d7e4e681fd53922f3f
stdeguzman <seantristan@kaist.ac.kr>
2020-06-08 19:38:12 +0900

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,27 +1,35 @@
 $( document ).ready(function() {	
 	var Logo = document.getElementById('logo');
-	var SideBar = document.getElementById('sideBarOpen');
-	var wholePage = document.getElementsByClassName('outside_topbar');
+	var SideBarOpen = document.getElementById('sideBarOpen');
+	var SideBarClose = document.getElementById('sideBarClose');
+	var ULContent = document.getElementsByTagName("UL")[0];
+	var SideBarWidth = document.getElementsByClassName("sidebar")[0];
 
-	function openSideBar() {
-
-	}
-
-	function closeSideBar() {
-		
-	}
+	var SideBarText = '<li><span class="col-md-2" id="logo"><b> WheelOn </b></span><span class="icon-star" id="sideBarClose" onclick="closeMenu()"></span></li><br><li class="options">Forum</li><li class="options">Routes</li><li class="options">Rate</li><li class="copyright"> WheelOnInThisTogether <br> Copyright 2020 </li>'
 
 	function bindEvents() {
 		Logo.onclick = function() {
-			console.log("Return to Homepage");
 			window.open("homepage.html", "_self");
 		}
-		SideBar.onclick = function() {
-			console.log("Open the sidebar");
-			// $(className).style.opacity = "0.1";
-			window.open("sidebar.html", "_self");
+
+		SideBarOpen.onclick = function() {
+			SideBarWidth.style.width = '20%';
+			ULContent.innerHTML = SideBarText;
 		}
+
+		$(document).on('click', '#sideBarClose', function () {
+			SideBarWidth.style.width = 0;
+			ULContent.innerHTML = "";
+		});
+
+		$(document).on('click', '.options', function() {
+			if (this.innerHTML == "Forum") {window.open("forum_homepage.html", "_self");}
+			else if (this.innerHTML == "Routes") { } // routes
+			else if (this.innerHTML == "Rate") {window.open("rates.html", "_self");}
+		});
 	}
 
+	document.getElementsByClassName("sidebar")[0].style.width = 0;
+	document.getElementsByTagName("UL")[0].innerHTML = "";
 	bindEvents();
 });
