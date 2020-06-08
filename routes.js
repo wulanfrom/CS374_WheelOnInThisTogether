a81bbd3785158bc00f543976157a922a33700751
Chrysan Angela <chrysan.angela@gmail.com>
2020-06-09 01:28:47 +0900

    modified routes.js

--- routes.js
+++ routes.js
@@ -3,6 +3,10 @@ $( document ).ready(function() {
   const dbRef = firebase.database().ref();
   const routesRef = dbRef.child('routes');
 
+  function loadCard(){
+    
+  }
+
   function searchRoute(){
     var from_value = $("#fromPlace").value;
     var to_value = $("#toPlace").value;
@@ -12,12 +16,27 @@ $( document ).ready(function() {
       snapshot.forEach(function(childSnapshot){
         var from_db = childSnapshot.val().from;
         var to_db = childSnapshot.val().to;
-        var landmarks_db = childSnapshot.val().landmarks;
-        var likes_db = childSnapshot.val().likes;
-        var title_db = childSnapshot.val().title;
-        var username = childSnapshot.val().username;
         if ((encodeURIComponent(from_value) == encodeURIComponent(from_db)) && (encodeURIComponent(to_value) == encodeURIComponent(to_db))){
-          dataList.push
+          var pushid = childSnapshot.key;
+          var landmarks_db = childSnapshot.val().landmarks;
+          var likes_db = childSnapshot.val().likes;
+          var title_db = childSnapshot.val().title;
+          var username_db = childSnapshot.val().username;
+          var description_db = childSnapshot.val().description;
+          var images_db = childSnapshot.val().images;
+
+          var descriptionList = [];
+          var imagesList = [];
+
+          for (var i in description_db){
+            descriptionList.push(description_db[i]);
+          }
+
+          for (var i in images_db){
+            imagesList.push(images_db[i]);
+          }
+
+          dataList.push([]);
         }
       })
     })
