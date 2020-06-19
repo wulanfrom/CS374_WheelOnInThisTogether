330bda28287e25ef9f5fce663fe836549214be2b
SEAN <seantristan@kaist.ac.kr>
2020-06-19 16:17:46 +0900

    2020/06/19 Commits

--- new_navbar.js
+++ new_navbar.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
     $('.nav-item .nav-link').on('click', function () {
         if (this.innerHTML == "Ask a Question") { console.log("Forum"); }
-        else if (this.innerHTML == "Suggest a Route") { console.log("Routes"); }
+        else if (this.innerHTML == "Look for Routes") { console.log("Routes"); }
         else if (this.innerHTML == "Rate a Place") { console.log("Rates"); }
         //window.open('htmlLink', '_self');
     });
-
-    $('.navbar-brand').on('click', function() {
-        window.open('new_homepage.html', '_self');
-    })
 });
