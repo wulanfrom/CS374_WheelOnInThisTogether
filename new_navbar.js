027755cd5e862813555b4d87adc427c813bc2aa2
wulanfrom <biasindi.wulanfrom@gmail.com>
2020-09-10 18:18:36 +0900

    cleaned the repository

--- new_navbar.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.nav-item .nav-link').on('click', function () {
-        if (this.innerHTML == "Ask a Question") { console.log("Forum"); }
-        else if (this.innerHTML == "Look for Routes") { console.log("Routes"); }
-        else if (this.innerHTML == "Rate a Place") { console.log("Rates"); }
-        //window.open('htmlLink', '_self');
-    });
-});
